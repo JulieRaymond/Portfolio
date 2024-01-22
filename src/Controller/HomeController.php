e0815b49110889f6a9890532df1beb5f894d94f5
Julie <raymondjulie@proton.me>
2024-01-22 14:10:04 +0100

    modals working ok

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\ProjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(ProjectRepository $projectRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        // Récupérez vos projets depuis la base de données
+        $projects = $projectRepository->findAll();
+
+        // Passez les projets à la vue Twig
+        return $this->render('home/index.html.twig', [
+            'projects' => $projects,
+        ]);
     }
 }
