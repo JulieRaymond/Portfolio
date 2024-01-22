13eccd79b999584c6e48499752fe5185473c4829
Julie <raymondjulie@proton.me>
2024-01-22 16:27:58 +0100

    easy admin crud working, can create edit and delete project

--- src/Controller/Admin/ProjectCrudController.php
+++ src/Controller/Admin/ProjectCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Project;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
@@ -15,10 +16,34 @@ class ProjectCrudController extends AbstractCrudController
         return Project::class;
     }
 
+    public function createEntity(string $entityFqcn): Project
+    {
+        $project = new Project();
+        $project->setAuthor($this->getUser());
+
+        return $project;
+    }
+
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        // Aucune action supplémentaire nécessaire lors de la mise à jour
+        parent::updateEntity($entityManager, $entityInstance);
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        // Aucune action supplémentaire nécessaire avant la persistance
+        parent::persistEntity($entityManager, $entityInstance);
+    }
+
+    public function deleteEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        // Aucune action supplémentaire nécessaire avant la suppression
+        parent::deleteEntity($entityManager, $entityInstance);
+    }
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
             TextField::new('title'),
             TextEditorField::new('description'),
             TextField::new('duration'),
